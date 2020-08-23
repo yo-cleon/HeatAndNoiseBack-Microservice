7cc2209497b2d58863be64e208b19e746aa061f1
yo-cleon <cleon.form@gmail.com>
2020-08-23 21:52:39 +0100

    Añadido microservicio Gateway que sustituye a Zuul

---
+++ gateway/src/main/java/com/example/microservices/app/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.microservices.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println(("Eureka gateway is working!"));
+	}
+
+}
